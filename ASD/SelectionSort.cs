818178cadc5d6b3c71124f15ec5d40c0cf2531e8
Horea Oros <horea.oros@gmail.com>
2015-01-29 09:51:54 +0200

    Add SelectionSort

---
+++ ASD/SelectionSort.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ASD
+{
+    class SelectionSort
+    {
+        public static void Main(string[] args)
+        {
+            string filename = "words3.txt";
+            string[] a = Util.readWords(filename);
+
+            sort(a);
+
+            Debug.Assert(isSorted(a), "Vectorul nu este sortat");
+
+            show(a);
+
+        }
+        /// <summary>
+        /// Selection sort. Toate elementele din stanga pozitiei i sunt ordonate si sunt cele mai mici i elemente din vector
+        /// </summary>
+        /// <param name="a"></param>
+        public static void sort(IComparable[] a)
+        {
+            int i, j;
+            int n = a.Length;
+            int min;
+            for (i = 0; i < n; i++)
+            {
+                min = i;
+                for (j = i + 1; j < n; j++)
+                    if (less(a[j], a[min]))
+                        min = j;
+                exch(a, i, min);
+            }
+        }
+        /// <summary>
+        /// Metoda privata ajutatoare pentru a determina daca un element este mai mic decat altul
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        private static bool less(IComparable p, IComparable q)
+        {
+            return p.CompareTo(q) < 0;
+        }
+        /// <summary>
+        /// Metoda privata ajutatoare care interschimba doua valori din vector
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        private static void exch(IComparable[] a, int i, int j)
+        {
+            IComparable t;
+            t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+        }
+        /// <summary>
+        /// Metoda privata ajutatoare care afiseaza elementele vectorului pe o singura linie
+        /// </summary>
+        /// <param name="a">Vector de elemente</param>
+        private static void show(IComparable[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("{0} ", a[i]);
+            }
+            Console.WriteLine();
+        }
+        /// <summary>
+        /// Metoda care determina daca vectorul este sortat
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static bool isSorted(IComparable[] a)
+        {
+            for (int i = 1; i < a.Length; i++)
+                if (less(a[i], a[i - 1]))
+                    return false;
+            return true;
+        }
+    }
+}
